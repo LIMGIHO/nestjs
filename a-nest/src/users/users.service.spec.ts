e91360828afb5bebe655614394909f9dd89071e5
Lim <lasid84@gmail.com>
2025-06-10 22:18:10 +0900

    GateWay, 파일업로드, 단위테스트

--- a-nest/src/users/users.service.spec.ts
+++ a-nest/src/users/users.service.spec.ts
@@ -1,12 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
+
+class MockUserRepository {
+  #data = [];
+  findOne() {}
+}
+class MockWorkspaceMembersRepository {}
+class MockChannelMembersRepository {}
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(Users),
+          useClass: MockUserRepository
+        },
+        {
+          provide: getRepositoryToken(WorkspaceMembers),
+          useClass: MockWorkspaceMembersRepository
+        },
+        {
+          provide: getRepositoryToken(ChannelMembers),
+          useClass: MockChannelMembersRepository
+        }
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -15,4 +40,13 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('findByEmail은 이메일을 통해 유저를 찾아야 함.', () => {
+    expect(service.findById('lasid84@naver.com')).resolves.toStrictEqual({
+      email: 'lasid84@naver.com',
+      id: 1
+    });
+  });
+
+  it.todo('해야할것 todo처리')
 });
